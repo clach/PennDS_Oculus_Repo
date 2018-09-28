ec70e70e56a42eef7c1231656c7da4ada874729b
clach <clach@seas.upenn.edu>
2018-09-28 13:50:34 -0400

    point and click working

---
+++ ImmersiveEnvironment/Assets/Scripts/LookAtMe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMe : MonoBehaviour {
+
+    public Camera cameraToLookAt;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cameraToLookAt.transform.position - v);
+        transform.Rotate(0, 180, 0);
+
+        // want to make it rotate in all directions
+
+    }
+}
